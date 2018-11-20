4d72c24e52e41af532bb6471db25d351f9332ba2
Jimmy Maple <38733055+jimmyatSplunk@users.noreply.github.com>
2018-11-20 10:03:27 -0500

    Update pwchange.sh

--- bin/pwchange.sh
+++ bin/pwchange.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-
+# Define the original and new passwords here
 OLDPASS=changeme
 NEWPASS=thisIsALongPassword123
 
+# Look for the checkpoint file and error out if it exists
 if [ -f $SPLUNK_HOME/etc/pwd_changed ]
 then
         echo `date -R` $HOSTNAME: Splunk account password was already changed.
         exit
 fi
-$SPLUNK_HOME/bin/splunk edit user admin -password $NEWPASS -auth admin:$OLDPASS
-CHANGED=`tail -n 10 $SPLUNK_HOME/var/log/splunk/splunkd.log | grep ExecProcessor | grep Login`
+
+# Change the password
+$SPLUNK_HOME/bin/splunk edit user admin -password $NEWPASS -auth admin:$OLDPASS > /dev/null 2>&1
+
+# Check splunkd.log for any error messages relating to login during the script and determine whether the change was successful or not
+CHANGED=`tail -n 10 $SPLUNK_HOME/var/log/splunk/splunkd.log | grep pwchange | grep Login`
 if [ -z "$CHANGED" ]
 then
 	echo `date -R` $HOSTNAME: Splunk account password successfully changed.
