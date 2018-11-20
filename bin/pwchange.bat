83778eb74469600edbbb69b52db694a8cc364a72
Jimmy Maple <38733055+jimmyatSplunk@users.noreply.github.com>
2018-11-20 10:03:00 -0500

    Update pwchange.bat

--- bin/pwchange.bat
+++ bin/pwchange.bat
@@ -1,22 +1,42 @@
 @echo off
+REM Define the original and new passwords here
 SET OLDPASS=changeme
-SET NEWPASS=thisIsALongPassword123
-SET CHECKPOINT="%SPLUNK_HOME%\etc\pwd_changed"
-IF EXIST %CHECKPOINT% (
-@echo on
-echo "%date% %time% %HOST%: Splunk account password was already changed."
-) ELSE (
+SET NEWPASS=veryLongPassword1234
+
+REM Other variables relating to the checkpoint file and the path to test login and hostname for logging
+SET CHECKPOINT=%SPLUNK_HOME%\etc\pwd_changed
+SET LOGIN_COMMAND="%SPLUNK_HOME%\bin\splunk.exe" login -auth admin:"%OLDPASS%"
 FOR /F "usebackq" %%i IN (`hostname`) DO SET HOST=%%i
-"%SPLUNK_HOME%\bin\splunk.exe" edit user admin -password %NEWPASS% -auth admin:%OLDPASS%
-FOR /F %%C in ('find /v /c "" ^< "%SPLUNK_HOME%\var\log\splunk\splunkd.log"') DO SET LINE_COUNT=%%C
-SET COUNT=%LINE_COUNT%-10
-SET FAILURE=`more +%COUNT% "%SPLUNK_HOME%\var\log\splunk\splunkd.log" | findstr ExecProcessor | findstr Login`
-IF EXIST %FAILURE% (
-	@echo on
-	echo "%date% %time% %HOST%: Splunk account login failed. Old password is not correct for this host"
+
+REM Look for the checkpoint file and decide to error or continue
+IF EXIST "%CHECKPOINT%" (
+	goto NOCHANGE
 ) ELSE (
-echo "%date% %time% %HOST%: Splunk account password successfully changed." > %CHECKPOINT%
-@echo on
-echo "%date% %time% %HOST%: Splunk account password successfully changed."
-	)
+	goto CHANGE
 )
+
+REM Test logging in and if it fails throw error otherwise change the password
+:CHANGE
+FOR /F "tokens=2 usebackq" %%C in (`"%LOGIN_COMMAND%"`) DO SET LOGIN=%%C
+IF NOT "%LOGIN%"=="Failed" ( 
+"%SPLUNK_HOME%\bin\splunk.exe" edit user admin -password "%NEWPASS%" >NUL
+) ELSE (
+	goto FAILED
+)
+
+REM Create the checkpoint file and log success
+:SUCCESS
+echo %date% %time% %HOST%: Splunk account password successfully changed. > "%CHECKPOINT%"
+echo %date% %time% %HOST%: Splunk account password successfully changed.
+exit
+
+REM Log failure
+:FAILED
+echo %date% %time% %HOST%: Splunk account login failed. Old password is not correct for this host.
+exit
+
+REM Log that the checkpoint file exists
+:NOCHANGE
+echo %date% %time% %HOST%: Splunk account password was already changed.
+exit
+
